55d1676dedc65cddd64ec06fc97e9c0e96057f9e
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-05-24 15:19:24 +0530

    airbnb project started

---
+++ react-js projects/airbnb-scrimba/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      Kushagra
+    </div>
+  );
+}
+
+export default App;
