d0a8dc2d8202815234ec9e1eda70ec1829b03933
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-05-26 12:53:36 +0530

    finished airbnb

--- react-js projects/airbnb-scrimba/src/App.js
+++ react-js projects/airbnb-scrimba/src/App.js
@@ -3,18 +3,48 @@ import Card from "./components/Card";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 
-import image1 from "./assets/image1.png";
-import image2 from "./assets/image2.png";
-import image3 from "./assets/image3.png";
+// import image1 from "./assets/image1.png";
+// import image2 from "./assets/image2.png";
+// import image3 from "./assets/image3.png";
+
+import data from "./data"
+
+console.log(data)
+
 
 function App() {
+
+  const cards = data.map((item) =>  {
+    return (
+      <Card
+                key={item.id}
+                // img={item.coverImg}
+                // rating={item.stats.rating}
+                // reviewCount={item.stats.reviewCount}
+                // location={item.location}
+                // title={item.title}
+                // price={item.price}
+                // openSpots={item.openSpots}
+
+                // passing whole object at once
+                item={item}
+
+                // using object spread syntax of es6
+                // {...item}
+
+            />
+    )
+  })
+
   return (
     <div className="App">
       <Navbar />
       <Main />
       <div className="cards-div">
-        <Card
-          img={image1}
+
+        {cards}
+        {/* <Card
+          // img={image1}
           rating={`"5.0"`}
           reviewCount={`(6)`}
           country={`India`}
@@ -22,7 +52,7 @@ function App() {
           price={136}
         />
         <Card
-          img={image2}
+          // img={image2}
           rating={`"5.0"`}
           reviewCount={`(6)`}
           country={`India`}
@@ -30,13 +60,13 @@ function App() {
           price={136}
         />
         <Card
-          img={image1}
+          // img={image1}
           rating={`"5.0"`}
           reviewCount={`(6)`}
           country={`India`}
           title={`Life Lessons with Katie Zaferes`}
           price={136}
-        />
+        /> */}
       </div>
     </div>
   );
--- react-js projects/airbnb-scrimba/src/components/Card.jsx
+++ react-js projects/airbnb-scrimba/src/components/Card.jsx
@@ -2,20 +2,33 @@ import React from "react";
 import star from "../assets/star.png";
 
 export default function Card(props) {
+
+  let badgeText
+  if (props.item.openSpots === 0) {
+      badgeText = "SOLD OUT"
+  } else if (props.item.location === "Online") {
+      badgeText = "ONLINE"
+  }
+  
   return (
     <div className="card">
-     <img className="cardImg" src={props.img} alt="" />
+     {/* {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>} */}
 
+     {/* {badgeText !== '' && <div className="card--badge">{badgeText}</div>} */}
+
+     {badgeText && <div className="card--badge">{badgeText}</div>}
+
+      <img className="cardImg" src={props.item.coverImg} alt="" />
       <div className="cardContent">
         <div className="flexRow">
           <img className="star flexRow" src={star} alt="" />
-          <span className="">{props.rating}</span>
-          <span className="gray">&nbsp;{props.reviewwCount} •</span>
-          <span className="gray">{props.country}</span>
+          <span className="">{props.item.stats.rating}</span>
+          <span className="gray">&nbsp;({props.item.stats.reviewCount})&nbsp;•&nbsp;</span>
+          <span className="gray">{props.item.location}</span>
         </div>
-        <p>{props.title}</p>
+        <p>{props.item.title}</p>
         <p>
-          <span className="bold">From ${props.price}</span> / person
+          <span className="bold">From ${props.item.price}</span> / person
         </p>
       </div>
     </div>
