32af27ab08f370f4508ee69f2b2404d18179520a
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-05-27 11:30:44 +0530

    c3

---
+++ react-js projects/meme-generator/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-js projects/meme-generator/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+export default function Navbar() {
+  return (
+    <nav className='nav' >
+        <img className='nav-logo' src={logo} />
+    </nav>
+  )
+}
