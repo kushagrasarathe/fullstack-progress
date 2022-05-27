28288e3f2fbe44f7d64a5b888fd878d09479487c
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-05-27 11:51:07 +0530

    c4

--- react-js projects/meme-generator/src/components/Navbar.jsx
+++ react-js projects/meme-generator/src/components/Navbar.jsx
@@ -5,6 +5,8 @@ export default function Navbar() {
   return (
     <nav className='nav' >
         <img className='nav-logo' src={logo} />
+        <h1 className='nav-title'>Meme Generator</h1>
+        <h4 className='nav-subtitle'>React Project</h4>
     </nav>
   )
 }
