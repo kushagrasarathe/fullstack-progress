e8dc0486b161407eafc5513b6722d491cbc6cf22
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-05-27 12:26:47 +0530

    form finished

--- react-js projects/meme-generator/src/App.js
+++ react-js projects/meme-generator/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Form from './components/Form';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="">
       <Navbar />
+      <Form />
     </div>
   );
 }
---
+++ react-js projects/meme-generator/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <div className="form">
+      <div className="form-input">
+        <input className="input" type={'text'} />
+        <input className="input" type={'text'} />
+      </div>
+        <button className="button">Get a new Image 🖼 </button>
+    </div>
+  );
+}
