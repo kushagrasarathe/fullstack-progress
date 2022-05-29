1604d8b178c399c5257fec1e0014af952319a77a
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-05-29 20:21:36 +0530

    c5

--- react-js projects/meme-generator/src/components/Form.jsx
+++ react-js projects/meme-generator/src/components/Form.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Form() {
+
+    function getMeme() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
   return (
     <div className="form">
       <div className="form-input">
         <input className="input" type={'text'} />
         <input className="input" type={'text'} />
       </div>
-        <button className="button">Get a new Image 🖼 </button>
+        <button className="button" onClick={getMeme}>Get a new Image 🖼 </button>
     </div>
   );
 }
