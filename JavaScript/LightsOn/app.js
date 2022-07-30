66a147575aea8e9aec351467c927dc9bfed7224c
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-07-30 21:41:20 +0530

    added function to turn lights onn/off

--- JavaScript/LightsOn/app.js
+++ JavaScript/LightsOn/app.js
@@ -1,6 +1,20 @@
 let state = false;
 
+let lightsOnn = document.getElementById("onn");
+let lightsOff = document.getElementById("off");
+
+lightsOff.src = "./off.jpg"
+
 function toggle() {
     state = !state;
+
+    if( state === true ) {
+        lightsOnn.style.display = "block";
+        lightsOff.style.display = "none";
+    } else {
+        lightsOnn.style.display = "none";
+        lightsOff.style.display = "block";
+    }
+
     console.log(state);
 }
