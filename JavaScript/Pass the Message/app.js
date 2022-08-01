e12047d7178e9a65bc37fbd7c8f6c5abb0e4f907
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-08-01 22:53:09 +0530

    pass the message javascript

---
+++ JavaScript/Pass the Message/app.js
@@ -0,0 +1,14 @@
+let msg = document.querySelector('#msg');
+let delivered_msg = document.querySelector('.message');
+let success = document.querySelector('.success');
+
+function submit() {
+    console.log(msg.value);
+    if ( msg.value ) {
+        delivered_msg.textContent = "Message Delivered";
+    } else {
+        delivered_msg.textContent = "Empty message, please enter desired mesasage above";
+    }
+    success.textContent = msg.value;
+}
+
