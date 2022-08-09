287bb63a359ed49161e1a2b83f45030e22276f20
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-08-09 23:22:45 +0530

    add to array function added

--- JavaScript/List Search/app.js
+++ JavaScript/List Search/app.js
@@ -1,3 +1,18 @@
-let input =  document.querySelector('.input');
+const input =  document.querySelector('.input');
+const add_btn = document.querySelector('.add_btn')
+const item = document.querySelector('.add')
+let data = [];
 
-input.addEventListener( 'keyup', search());
+const addToList = () => {
+    let newItem = item.value;
+    data.push(newItem);
+    console.log(data)
+    console.log(newItem);
+}
+
+const search = () => {
+    console.log('test');
+}
+
+input.addEventListener( 'keyup', search);
+add_btn.addEventListener( "click", addToList);
