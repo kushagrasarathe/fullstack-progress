1f4753b6027b4935858c510fb32ff10937a0258f
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-08-10 22:20:43 +0530

    test

--- JavaScript/List Search/app.js
+++ JavaScript/List Search/app.js
@@ -1,13 +1,24 @@
 const input =  document.querySelector('.input');
 const add_btn = document.querySelector('.add_btn')
 const item = document.querySelector('.add')
+const list = document.querySelector(".list");
 let data = [];
 
 const addToList = () => {
     let newItem = item.value;
     data.push(newItem);
-    console.log(data)
-    console.log(newItem);
+
+      
+    data.forEach((item)=>{
+        let create_item = document.createElement("h1");
+        create_item.innerText = item;
+      list.appendChild(create_item);
+    })  
+
+    // create_item.appendChild(document.createTextNode(newItem));
+    // list.appendChild(create_item);
+    // console.log(data)
+    // console.log(newItem);
 }
 
 const search = () => {
