baf23d2e0b8064f4748cb4b82e24911d231aef7a
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-08-15 17:06:00 +0530

    password generator

---
+++ JavaScript/Random Password Generator/app.js
@@ -0,0 +1,120 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let first_pass = document.querySelector(".one");
+let second_pass = document.querySelector(".two");
+let generate_btn = document.querySelector(".btn");
+
+let pwd_one = [];
+let pwd_two = [];
+
+let arr_len = characters.length;
+
+function generatePassword() {
+  for (let i = 1; i <= 15; i++) {
+    first_pass.textContent = pwd_one;
+
+    let random1 = Math.floor(Math.random() * arr_len);
+    let random2 = Math.floor(Math.random() * arr_len);
+
+    pwd_one.push(characters[random1]);
+    pwd_two.push(characters[random2]);
+    // console.log(pwd_one);
+    first_pass.textContent = pwd_one.join("");
+
+    second_pass.textContent = pwd_two.join("");
+  }
+}
+
+generate_btn.addEventListener("click", generatePassword);
