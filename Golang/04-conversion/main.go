55e229c7a1f4315de011f93b28a81e49928efe2a
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-01 17:23:50 +0530

    error handling and type conversion in golang

---
+++ Golang/04-conversion/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main()  {
+	fmt.Println("Welcomme to pizza app")
+	
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please rate the pizza b/w 1 & 5")
+	
+	input, _ := reader.ReadString('\n')
+	
+	fmt.Println("Thanks for rating", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	// error handling
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to rating: ", numRating + 1)
+	}
+}
