b3321e280fcf35cb02eb2e13b5df3a2c30f4a379
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-11-23 23:45:57 +0530

    promise api (promise.all)

--- JavaScript/Practice/app.js
+++ JavaScript/Practice/app.js
@@ -24,24 +24,67 @@
   
   // Promise chaining
   
+  // let p1 = new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     console.log("Resolved after 2 seconds")
+  //     resolve(03)
+  //   }, 2000)
+  // })
+  
+  // p1.then((value) => {
+  //   console.log(value)
+  //   let p2 = new Promise((resolve, reject) => {
+  //       setTimeout(() => {
+  //           resolve("Promise 2") 
+  //           console.log("Resolved after 5 seconds")
+  //       }, 5000);
+  //   })
+  //   return p2;
+  // }).then((value) => {
+  //   console.log("Promise execution done")
+  // }).then((value) => {
+  //   console.log("100% execution done")
+  // })
+
+
+
+
+  // Promise API
+
   let p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("Resolved after 2 seconds")
-      resolve(03)
-    }, 2000)
+      resolve("Value 1")
+    }, 1000);
   })
-  
-  p1.then((value) => {
+
+  let p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Value 2")
+      // reject(new Error("Error"))
+    }, 2000);
+  })
+
+  let p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Value 3")
+    }, 3000);
+  })
+
+  // p1.then((value) => {
+  //   console.log(value)
+  // })
+
+  // p2.then((value) => {
+  //   console.log(value)
+  // })
+
+  // p3.then((value) => {
+  //   console.log(value)
+  // })
+
+  // promise.all waits for all promises to resolve and returns a array of their results.
+  // if any promise fails, then it becomes a error and all other results are ignored
+  const promise_all = Promise.all([p1, p2, p3])
+   promise_all.then((value) => {
     console.log(value)
-    let p2 = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Promise 2") 
-            console.log("Resolved after 5 seconds")
-        }, 5000);
-    })
-    return p2;
-  }).then((value) => {
-    console.log("Promise execution done")
-  }).then((value) => {
-    console.log("100% execution done")
   })
--- JavaScript/README.md
+++ JavaScript/README.md
@@ -461,6 +461,77 @@ prom.catch((value) => {
 
 ### Promise chaining
 
+Promise chaining allows us to chain multiple asynchronous tasks in a specific order. This is useful when dealing with complex code where one asynchronous task is to be performed after completion of another asynchronous task.
 
+```js
+  let p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Resolved after 2 seconds")
+      resolve(03)
+    }, 2000)
+  })
+  
+  p1.then((value) => {
+    console.log(value)
+    let p2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Promise 2") 
+            console.log("Resolved after 5 seconds")
+        }, 5000);
+    })
+    return p2;
+  }).then((value) => {
+    console.log("Promise execution done")
+  }).then((value) => {
+    console.log("100% execution done")
+  })
+```
+
+### Promise API
+
+**Promise.all()** waits for all promises to resolve and returns a array of their results. If any promise fails, then it becomes a error and all other results are **ignored**
+
+```js
+
+  let p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Value 1")
+    }, 1000);
+  })
+
+  let p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Value 2")
+      // reject(new Error("Error"))
+    }, 2000);
+  })
+
+  let p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Value 3")
+    }, 3000);
+  })
+
+  // p1.then((value) => {
+  //   console.log(value)
+  // })
+
+  // p2.then((value) => {
+  //   console.log(value)
+  // })
+
+  // p3.then((value) => {
+  //   console.log(value)
+  // })
+
+  // promise.all waits for all promises to resolve and returns a array of their results.
+  // if any promise fails, then it becomes a error and all other results are ignored
+  const promise_all = Promise.all([p1, p2, p3])
+   promise_all.then((value) => {
+    console.log(value)
+  })
+
+
+```
 
 ## Async/Await
