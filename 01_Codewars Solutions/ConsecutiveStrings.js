031c504ff61a496067e667b1532358e31a694c9d
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-12-15 23:23:08 +0530

    codewars solution

---
+++ 01_Codewars Solutions/ConsecutiveStrings.js
@@ -0,0 +1,42 @@
+// You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+// Examples:
+
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so 
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+// Note
+
+// consecutive strings : follow one after another without an interruption
+
+
+function longestConsec(strarr, k) {
+    if (strarr.length == 0 || k > strarr.length || k <= 0) return '';
+    
+    let longStr = '';
+    
+    let newStr = '';
+    
+    for (let i = 0; i < strarr.length; i++){
+      newStr = strarr.slice(i, i+k);
+      if (newStr.join('').length > longStr.length ){
+        longStr = newStr.join('');
+      }
+    }
+    
+    return longStr;
+}
