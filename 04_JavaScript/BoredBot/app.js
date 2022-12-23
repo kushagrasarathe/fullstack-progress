4c3f1ed388090aa29dcabd977b6a26596c3e85e8
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-12-23 22:20:12 +0530

    boredbot finished

--- 04_JavaScript/BoredBot/app.js
+++ 04_JavaScript/BoredBot/app.js
@@ -1,8 +1,10 @@
-fetch("https://apis.scrimba.com/bored/api/activity")
-  .then((response) => response.json())
-  .then((data) => {
-      document.getElementById("container").innerHTML = `
-      <p>${data.activity}</p>
-      `
-  });
-  
+document.querySelector(".call-api-btn").addEventListener("click", () => {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((response) => response.json())
+    .then((data) => {
+      //   document.getElementById("container").innerHTML = `
+      //   <p>${data.activity}</p>
+      //   `
+      document.getElementById("activity").textContent = data.activity;
+    });
+});
