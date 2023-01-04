df68673811a1fd2851cb947527be84bbc3713e21
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2023-01-04 20:54:11 +0530

    blogspace: create new post

--- 04_JavaScript/20_BlogSpace/app.js
+++ 04_JavaScript/20_BlogSpace/app.js
@@ -2,37 +2,57 @@
 const postTitle = document.querySelector("#post_title");
 const postBody = document.querySelector("#post_body");
 
+let postArray = [];
+const renderPost = function () {
+  let html = "";
+  for (let post of postArray) {
+    html += `
+      <div class="mt-4  ">
+
+          <h2 class="text-lg font-semibold">${post.title}</h2>
+          <p class="mt-2">${post.body}</p>
+          <hr class="mt-3 border-gray-500 " />
+          </div>
+      `;
+  }
+  document.getElementById("blogs_el").innerHTML = html;
+};
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts", { method: "GET" })
   .then((res) => res.json())
   .then((data) => {
-    const dataArr = data.slice(0, 5);
+    postArray = data.slice(0, 5);
+    renderPost();
+
+    // another approach
     // const posts = dataArr.map((post) => {
     //   return `<div>
     //         <h1>${post.title}</h1>
     //         <p>${post.body}</p>
     //     </div>`;
     // });
-
-    let html = "";
-    for (let post of dataArr) {
-      html += `
-        <div class="mt-4  ">
-
-            <h2 class="text-lg font-semibold">${post.title}</h2>
-            <p class="mt-2">${post.body}</p>
-            <hr class="mt-3 border-gray-500 " />
-            </div>
-        `;
-    }
     // document.getElementById("blogs_el").innerHTML = posts;
-    document.getElementById("blogs_el").innerHTML = html;
   });
 
 document.querySelector("#new_post").addEventListener("submit", (event) => {
-  const newPostObj = {
+  const data = {
     title: postTitle.value,
     body: postBody.value,
   };
-  console.log(newPostObj);
+  console.log(data);
   event.preventDefault();
+
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      postArray.unshift(data)
+      renderPost()
+    });
 });
