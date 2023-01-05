62c93427c40b0c48869a98f173b757a000d3f074
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2023-01-05 22:49:20 +0530

    alchemy: remove occurrences problem

--- 01_Codewars Solutions/00_Alchemy University.js
+++ 01_Codewars Solutions/00_Alchemy University.js
@@ -190,3 +190,15 @@ function addOne(arr) {
     arr[i] = arr[i] + 1;
   }
 }
+
+// 🏁 Your Goal: Remove the Occurrences
+// Given an array of integers and a number, num, find all the occurrences of the number and remove it from the array.
+// Modify the array directly and do not return anything from this function.
+
+function removeOccurrences(arr, num) {
+  for (i = arr.length; i--; ) {
+    if (arr[i] === num) {
+      arr.splice(i, 1);
+    }
+  }
+}
