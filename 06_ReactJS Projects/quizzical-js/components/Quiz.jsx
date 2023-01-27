ae51b045ba3e1ff16ab4e0b0fc909ce35699aec0
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2023-01-27 17:29:44 +0530

    code refactored and added css

--- 06_ReactJS Projects/quizzical-js/components/Quiz.jsx
+++ 06_ReactJS Projects/quizzical-js/components/Quiz.jsx
@@ -1,108 +1,142 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Quiz(props) {
+  const [userSelected, setUserSelected] = useState([]);
+  const [highlight, setHighlight] = useState(false);
 
-    // {
-    //     quiz1: '',
-    //     quiz2: '',
-    //     quiz3: '',
-    //     quiz4: '',
-    //     quiz5: '',
+  //   useEffect ( () => {
+  //     submitAnswers()
+  //   }, [props.isCorrect])
+
+  let userSelectedOptions = [];
+  let correctOptionsArr = [];
+  let setBool = [];
+
+  function saveUserSelectedOptions(position, selectedOption, correctAnswer) {
+    userSelectedOptions[position] = {
+      id: position,
+      option: selectedOption,
+      isSelected: true,
+    };
+    correctOptionsArr[position] = {
+      id: position,
+      option: correctAnswer,
+      isSelected: false,
+    };
+    // setUserSelected((prev) => [
+    //   {
+    //     id: position,
+    //     option: selectedOption,
+    //     isSelected: true,
+    //   },
+    // ]);
+    // setActive(true);
+    return [userSelectedOptions, correctOptionsArr];
+  }
+
+  function submitAnswers() {
+    const arr = saveUserSelectedOptions();
+    let submitted = arr[0];
+    let answers = arr[1];
+
+    submitted.map((obj, index) => {
+      if (obj.option === answers[index].option) {
+        props.setScore((prevScore) => prevScore + 1);
+        setBool[index] = "correct";
+        props.setIsCorrect(setBool);
+      } else {
+        // props.setIsCorrect("wrong")
+        setBool[index] = "wrong";
+        props.setIsCorrect(setBool);
+      }
+      // : console.log(
+      //     `Option ${index + 1} is wrong, correct answer is ${
+      //       answers[index].option
+      //     }`
+      //   );
+    });
+
+    console.log(userSelected);
+
+    // for (let i = 0; i < 5; i++) {
+    //   if (submitted[i].option ) {
+    //     submitted[i].option === answers[i].option
+    //       ? props.setScore((prevScore) => prevScore + 1)
+    //       : console.log(
+    //           `Option ${i + 1} is wrong, correct answer is ${answers[i].option}`
+    //         );
+    //   } else if(submitted[i].option === undefined) {
+    //     console.log(`You didn't choose an option for question ${i + 1}`);
+    //   }
     // }
-    // console.log(props.quiz)
+  }
 
+  return (
+    <div className="pb-5 leading-6 tracking-wide">
+      <div className="">
+        {props.quizArr.map((quiz, index) => {
+          return (
+            <div className=" my-4 " key={index}>
+              <div className=" flex justify-between flex-wrap items-center">
+                <h3 className=" m-2  text-lg font-normal ">{quiz.question}</h3>
+                <span className=" text-base">{props.isCorrect[index]}</span>
+              </div>
+              <div className=" flex justify-between items-stretch flex-wrap">
+                {quiz.options.map((option, idx) => {
+                  return (
+                    <div
+                      onClick={() => {
+                        saveUserSelectedOptions(
+                          index,
+                          option,
+                          quiz.correct_answer
+                        );
+                      }}
+                      className={`
+                      border
+                       border-[#4D5B9E]
+                     text-black px-3 py-2 rounded-md md:w-64 w-36 m-2 mt-4 cursor-pointer hover:bg-blue-300 bg-[#D6DBF5] `}
+                      //  [#D6DBF5]
+                      key={idx}
+                    >
+                      {option
+                        .replace(/&#039;/g, "'")
+                        .replace(/&#62;/g, ">")
+                        .replace(/&#60;/g, "<")
+                        .replace(/&apos;/g, "'")
+                        .replace(/&amp;/g, "&")
+                        .replace(/&quot;/g, '"')
+                        .replace(/&apos;/g, "'")
+                        .replace(/&gt;/g, ">")
+                        .replace(/&lt;/g, "<")}
+                    </div>
+                  );
+                })}
+              </div>
+              <hr className=" border-t m-2 mt-5" />
 
-
-    // function saveUserSelectedOptions(position, selectedOption) {
-    //     setUserSelectedArr[position] = selectedOption
-    //     // arr.splice(2, 0, "Lene");
-    // }
-
-    let setUserSelectedArr = []
-    let setCorrectAnswers = [];
-
-    function saveUserSelectedOptions(position, selectedOption, correctAnswer) {
-        setUserSelectedArr[position] = selectedOption
-        setCorrectAnswers[position] = correctAnswer
-
-        return [setUserSelectedArr, setCorrectAnswers]
-        // console.log(object);
-    }
-
-
-    function submitAnswers() {
-        const arr = saveUserSelectedOptions()
-        // console.log(arr)
-        let submitted = arr[0]
-        let answers = arr[1]
-
-        // console.log(answers);
-
-        for (let i = 0; i < 5; i++) {
-            if (submitted[i]) {
-
-                submitted[i] === answers[i] ?
-                    props.setScore(prevScore => prevScore + 1) :
-                    console.log(`Option ${i + 1} is wrong, correct answer is ${answers[i]}`) 
-            } else {
-                console.log(`You didn't choose an option for question ${i + 1}`);
-            }
-        }
-
-
-        // if (submitted[0] === answers[0]) {
-        //     console.log("Quiz 1 Correct")
-        // } else
-        //     if (submitted[1] === answers[1]) {
-        //         console.log("Quiz 2 Correct")
-        //     } else
-        //         if (submitted[2] === answers[2]) {
-        //             console.log("Quiz 3 Correct")
-        //         } else
-        //             if (submitted[3] === answers[3]) {
-        //                 console.log("Quiz 4 Correct")
-        //             } else
-        //                 if (submitted[4] === answers[5]) {
-        //                     console.log("Quiz 5 Correct")
-        //                 }
-
-    }
-    // console.log(score)
-
-    // setInterval(() => console.log("User SWelected Options: " + setUserSelectedArr, "\n" + "Correct Options: " + setCorrectAnswers), 15000)
-
-    return (
-        <div>
-            <div>
-                {
-                    props.quizArr.map((quiz, index) => {
-                        // console.log(quiz)
-                        return <div key={index}>
-                            <h1>{quiz.question}</h1>
-                            <div className=' flex flex-col justify-center items-start flex-wrap'>
-                                {quiz.options.map((option, idx) => {
-                                    return <div
-                                        onClick={() => saveUserSelectedOptions(index, option, quiz.correct_answer)}
-                                        className={` bg-purple-500 px-3 py-2 rounded-md w-64 m-2 cursor-pointer hover:bg-purple-400`}
-                                        key={idx}>{option}</div>
-                                })}
-                            </div>
-                        </div>
-                    })
-                }
             </div>
-            <div>
-                <button
-                    onClick={submitAnswers}
-                    type="button" className="my-4 mx-3 inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out">Submit Answers</button>
-            </div>
-            <div>
-                <h2>Score:{props.score}</h2>
-            </div>
-
-            <button
-            onClick={props.resetGame}
-            type="button" className="my-4 mx-3 inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out">Reset Game</button>
-        </div>
-    )
+            
+          );
+        })}
+      </div>
+      <div className="m-2 my-5 flex justify-start items-center">
+        <button
+          onClick={submitAnswers}
+          type="button"
+          className="inline-block px-6 py-2.5 bg-[#4D5B9E] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#3d4d9c] hover:shadow-lg focus:bg-[#3d4d9c] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          Submit Answers
+        </button>
+        {/* <h2>Score: {props.score}</h2> */}
+        <button
+          onClick={props.resetGame}
+          type="button"
+          className="my-4 mx-4 inline-block px-6 py-2.5 bg-[#4D5B9E] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#3d4d9c] hover:shadow-lg focus:bg-[#3d4d9c] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          Reset Game
+        </button>
+      </div>
+    </div>
+  );
 }
--- 06_ReactJS Projects/quizzical-js/pages/_app.js
+++ 06_ReactJS Projects/quizzical-js/pages/_app.js
@@ -1,5 +1,27 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <div>
+        <div className="area" >
+          <ul className="circles"> */}
+            <Component {...pageProps} />
+            {/* <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div >
+      </div> */}
+    </>
+  )
+
 }
